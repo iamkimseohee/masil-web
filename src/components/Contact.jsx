c9a716c0c7c104e4b12b2f3a22e2e797f38f052d
iamkimseohee <kimd5ri@gmail.com>
2024-03-20 15:14:41 +0900

    240320

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -94,8 +94,8 @@ const Contact = () => {
           if (isMobile) {
             movePage("/success");
           } else {
-            alert("감사합니다. 마실에 제안 및 문의 주셔서 감사합니다. 보내주신 내용은 담당자가 검토하여 필요시 회신 드리도록 하겠습니다.");
-            // window.location.reload(); // 페이지 새로고침
+            alert("마실에 제안 및 문의 주셔서 감사합니다. 보내주신 내용은 담당자가 검토하여 필요시 회신 드리도록 하겠습니다.");
+            window.location.reload(); // 페이지 새로고침
           }
           console.log("Data inserted into table A:", insertData);
         } else {
@@ -107,8 +107,8 @@ const Contact = () => {
           if (isMobile) {
             movePage("/success");
           } else {
-            alert("감사합니다. 마실에 제안 및 문의 주셔서 감사합니다. 보내주신 내용은 담당자가 검토하여 필요시 회신 드리도록 하겠습니다.");
-            // window.location.reload(); // 페이지 새로고침
+            alert("마실에 제안 및 문의 주셔서 감사합니다. 보내주신 내용은 담당자가 검토하여 필요시 회신 드리도록 하겠습니다.");
+            window.location.reload(); // 페이지 새로고침
           }
           console.log("Data inserted into table B:", insertData);
         }
